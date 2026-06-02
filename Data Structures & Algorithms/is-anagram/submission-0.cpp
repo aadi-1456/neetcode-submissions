08b0d6835e2421ae39a9ed48cf7e6cd6e8cd3b51
Aditya Lal <79254817+aadi-1456@users.noreply.github.com>
2026-06-02 23:16:47 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    // Brute Force Solution
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        for(int i=0;i<s.length();i++){
+            if(s[i]!=t[i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
