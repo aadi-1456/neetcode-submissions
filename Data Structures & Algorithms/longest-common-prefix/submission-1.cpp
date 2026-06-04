2019a235eb916393f9fe21c34ad89471e7b0357e
Aditya Lal <79254817+aadi-1456@users.noreply.github.com>
2026-06-04 23:11:19 +0530

    Clarify substr behavior in longestCommonPrefix
    Added a comment to clarify the behavior of substr in the longestCommonPrefix function.

--- Data Structures & Algorithms/longest-common-prefix/submission-1.cpp
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<strs[0].length();i++){
             for(string & s: strs){
                 if(i==s.length() || s[i]!=strs[0][i]){
-                    return s.substr(0,i);
+                    return s.substr(0,i); // note if i==5 then the substr is of index 0,1,2,3,4
                 }
             }
         }
